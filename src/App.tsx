9f3ddf91b563180c02de5688bc3d2cf36828a535
Guillaume <guillaumelamarche1@gmail.com>
2022-06-14 15:02:56 -0400

    style: styling of To do

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   const toDos: ITask[] = [];
 
   const [taskName, setTaskName] = useState<string>("");
-  const [deadline, setDeadline] = useState<number | null>(null);
+  const [deadline, setDeadline] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>(() => {
     return JSON.parse(localStorage.getItem("toDos") || "[]") as ITask[];
   });
@@ -59,7 +59,7 @@ function App() {
     };
     setTodoList([newTask, ...todoList]);
     setTaskName("");
-    setDeadline(null);
+    setDeadline(0);
   };
 
   const onDeleteHandler = (id: number) => {
@@ -77,39 +77,42 @@ function App() {
       {/* <RecipeList recipes={recipes}></RecipeList> */}
       <Form onSubmit={onSubmitHandler}>
         <FormEntry className="inputTaskName">
-          <label htmlFor="task">Task</label>
           <input
             type="text"
             name="task"
             id="task"
             value={taskName}
             onChange={onChangeHandler}
+            placeholder="Enter task"
+            required
           />
         </FormEntry>
         <FormEntry className="inputDeadline">
-          <label htmlFor="deadline">Deadline (Days)</label>
           <input
             type="number"
             name="deadline"
             id="deadline"
             onChange={onChangeHandler}
+            placeholder="Enter deadline (in days)"
+            value={deadline}
+            required
           />
         </FormEntry>
-        <button type="submit">Add</button>
+        <AddButton type="submit">Add</AddButton>
       </Form>
-      <ul>
+      <ListTodo>
         {todoList.map((task: ITask, key: number) => {
           return (
-            <li key={key}>
+            <ListTodoItem key={key}>
               <TaskItem
                 task={task}
                 onDelete={onDeleteHandler}
                 onEdit={onEditHandler}
               />
-            </li>
+            </ListTodoItem>
           );
         })}
-      </ul>
+      </ListTodo>
     </AppContainer>
   );
 }
@@ -118,21 +121,76 @@ export default App;
 
 const AppContainer = styled.div`
   text-align: center;
+  color: var(--text-color);
 `;
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  padding: 20px;
+  justify-content: space-around;
+  border: 2px solid var(--tertiary-color);
+  border-radius: 10px;
+  width: 30%;
+  margin: 50px auto;
+  height: 150px;
+  padding: 10px 0px;
 `;
 const FormEntry = styled.div`
-  padding: 5px;
+  & input {
+    padding: 5px;
+    border-radius: 4px;
+    border: 2px solid var(--secondary-color);
+  }
+`;
+const AddButton = styled.button`
+  border-radius: 5px;
+  cursor: pointer;
+  border: none;
+  background-color: var(--secondary-color);
+  padding: 5px 50px;
+  color: var(--text-color);
+  font-size: 1.2rem;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: var(--secondary-color);
+    color: var(--text-color);
+    transform: scale(1.02);
+  }
+`;
+const ListTodo = styled.ul`
+  width: 80%;
+  margin: 0 auto;
+  padding: 0;
+  list-style: none;
 
-  & label {
-    padding: 0px 5px;
-    ::after {
-      content: " :";
+  &::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 2px;
+    background-color: var(--secondary-color);
+    margin-bottom: 30px;
+  }
+  &::after {
+    content: "";
+
+    display: block;
+    width: 100%;
+    height: 2px;
+    background-color: var(--secondary-color);
+    margin-top: 30px;
+  }
+`;
+const ListTodoItem = styled.li`
+  text-align: center;
+  &:not(:first-child) {
+    &::before {
+      content: "";
+      display: block;
+      width: 60%;
+      margin: 0 auto;
+      height: 1px;
+      background-color: var(--tertiary-color);
     }
   }
 `;
--- src/Components/TaskItem.tsx
+++ src/Components/TaskItem.tsx
@@ -15,7 +15,7 @@ export const TaskItem: React.FC<TaskItemProps> = ({
 }: TaskItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const [editedTaskName, setEditedTaskName] = useState("");
-  const [editedDeadline, setEditedDeadline] = useState<number | null>(null);
+  const [editedDeadline, setEditedDeadline] = useState<number>(0);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.name === "task") {
@@ -27,10 +27,24 @@ export const TaskItem: React.FC<TaskItemProps> = ({
   return (
     <>
       <TaskItemStyled>
-        <p>{`Task: ${task.name}`}</p>
-        <p>{`Deadline (Days): ${task.deadline}`}</p>
-        <button onClick={() => onDelete(task.id)}>Delete</button>
-        <button onClick={() => setIsEditing(!isEditing)}>Edit</button>
+        {!isEditing && (
+          <TaskInfo>
+            <p>{`Task: ${task.name}`}</p>
+            <p>{`Deadline (Days): ${task.deadline}`}</p>
+            <ButtonGroup>
+              <EditButton
+                onClick={() => setIsEditing(!isEditing)}
+                type="button"
+              >
+                Edit
+              </EditButton>
+              <DeleteButton onClick={() => onDelete(task.id)} type="button">
+                <span>X</span>
+              </DeleteButton>
+            </ButtonGroup>
+          </TaskInfo>
+        )}
+
         {isEditing && (
           <Form
             onSubmit={(e) => {
@@ -44,26 +58,35 @@ export const TaskItem: React.FC<TaskItemProps> = ({
             }}
           >
             <FormEntry className="inputTaskName">
-              <label htmlFor="task">Task</label>
               <input
                 type="text"
                 name="task"
                 id="task"
                 value={editedTaskName}
                 onChange={onChangeHandler}
-                placeholder={task.name}
+                placeholder={`Task: ${task.name}`}
+                required
               />
             </FormEntry>
             <FormEntry className="inputDeadline">
-              <label htmlFor="deadline">Deadline (Days)</label>
               <input
                 type="number"
                 name="deadline"
                 id="deadline"
                 onChange={onChangeHandler}
+                placeholder={`Deadline (Days): ${task.deadline}`}
+                required
               />
             </FormEntry>
-            <button type="submit">Save</button>
+            <ButtonGroup>
+              <EditButton
+                onClick={() => setIsEditing(!isEditing)}
+                type="button"
+              >
+                Cancel
+              </EditButton>
+              <SaveButton type="submit">Save</SaveButton>
+            </ButtonGroup>
           </Form>
         )}
       </TaskItemStyled>
@@ -74,16 +97,21 @@ const TaskItemStyled = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
-  border: 1px solid black;
+  justify-content: space-around;
   width: fit-content;
-  margin: 0 auto;
-  padding: 5px 10px;
   border-radius: 10px;
-
+  margin: auto;
   & p {
     padding: 0px 10px;
   }
+`;
+
+const TaskInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 2px 0px;
 `;
 
 const FormEntry = styled.div`
@@ -101,5 +129,68 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+  padding: 10px;
+  height: 110px;
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+const DeleteButton = styled.button`
+  border-color: var(--tertiary-color);
+  background-color: var(--background-color);
+  border-radius: 50%;
+  padding: 0;
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+  &:hover {
+    width: 50px;
+    aspect-ratio: unset;
+    border-radius: 5px;
+    padding: 5px 0px;
+    background-color: var(--secondary-color);
+    border: none;
+    color: var(--text-color);
+  }
+  &:hover span {
+    display: none;
+  }
+  &:hover::before {
+    content: "Delete";
+  }
+`;
+const EditButton = styled.button`
+  border-radius: 5px;
+  cursor: pointer;
+  border: none;
+  background-color: var(--secondary-color);
+  padding: 5px 10px;
+  color: var(--text-color);
+  transition: all 0.2s ease-in-out;
+  margin: 0px 5px;
+  &:hover {
+    background-color: var(--secondary-color);
+    color: var(--text-color);
+    transform: scale(1.02);
+  }
+`;
+const SaveButton = styled.button`
+  border-radius: 5px;
+  cursor: pointer;
+  border: none;
+  background-color: var(--secondary-color);
+  padding: 5px 10px;
+  color: var(--text-color);
+  transition: all 0.2s ease-in-out;
+  margin: 0px 5px;
+  &:hover {
+    background-color: var(--secondary-color);
+    color: var(--text-color);
+    transform: scale(1.02);
+  }
 `;
